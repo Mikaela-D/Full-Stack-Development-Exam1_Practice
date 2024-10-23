640ea106f47ed7817f36a73ff878bbb9adaf7743
Mikaela Diaz <mikaela.diaz@genesys.com>
2024-10-23 11:54:00 +0100

    Moved description to the right

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -8,23 +8,25 @@ function MeetupDetail(props) {
       </div>
 
       <div className={classes.main}>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-
         <div className={classes.details}>
-          <h2>Description</h2>
-          <p>{props.description}</p>
+          <div className={classes.descriptionContainer}>
+            <div className={classes.image}>
+              <img src={props.image} alt={props.title} />
+            </div>
+            <div className={classes.textContainer}>
+              <h2>Description</h2> {/* Move this into textContainer */}
+              <p>{props.description}</p>
+              <div className={classes.addressContainer}>
+                <p dangerouslySetInnerHTML={{ __html: props.address }} />
+              </div>
+            </div>
+          </div>
 
-          <div className={classes.addressContainer}>
-            <p dangerouslySetInnerHTML={{ __html: props.address }} />
+          <div className={classes.footer}>
+            <div>Tel: {props.telephone}</div>
+            <div>Email: {props.email}</div>
           </div>
         </div>
-      </div>
-
-      <div className={classes.footer}>
-        <div>Tel: {props.telephone}</div>
-        <div>Email: {props.email}</div>
       </div>
     </section>
   );
